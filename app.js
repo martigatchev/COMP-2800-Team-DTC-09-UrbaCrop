1142358e8bd893e5afa1234b7787310da7bf2827
Ryan Odribege <rodribege@gmail.com>
2021-05-12 10:57:23 -0700

    Added files that should hopefully allow our app to be hosted on Heroku

---
+++ app.js
@@ -0,0 +1,14 @@
+const { render } = require("ejs");
+const express = require("express");
+
+let app = express();
+    app.use(express.urlencoded({extended: true}));
+    app.use(express.static("public"));
+    app.set("view engine", "ejs");
+    
+let PORT = process.env.PORT || 3000;
+app.listen(PORT);
+
+app.get("/", (req, res)=> res.render("pages/index"));
+
+app.get("/gardener_profile", (req, res)=> res.render("gardener_profile"));
