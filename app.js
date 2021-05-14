bb9badd834bd75b897700cf9c8423bb01a4a825e
Ryan Odribege <rodribege@gmail.com>
2021-05-14 15:07:05 -0700

    rolling back app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const { render } = require("ejs");
 const express = require("express");
-const favicon = require('serve-favicon');
+
 
 
 let app = express();
@@ -8,7 +8,6 @@ let app = express();
     app.use("/public", express.static('public')); 
     app.use("/styles", express.static("styles"));
     app.use("/images", express.static("images"));
-    app.use(favicon(path.join(__dirname,'public','favicon-package','favicon.ico')));
     app.set("view engine", "ejs");
 
     
