ec4140a7302cd4f62424ad686e02a3823ec72636
Ryan Odribege <rodribege@gmail.com>
2021-05-14 14:02:28 -0700

    another attempt at favicon

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let app = express();
     app.use(express.urlencoded({extended: true}));
     app.use("/styles", express.static("styles"));
     app.use("/images", express.static("images"));
-    app.use('/favicon_package', express.static('favicon.ico'));
+    app.use(express.favicon('./public/favicon_package/favicon.ico'));
     app.set("view engine", "ejs");
 
     
