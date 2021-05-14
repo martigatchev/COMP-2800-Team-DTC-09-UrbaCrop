b23e555d224c5425a5d0b9e1979fd9b3303bcb50
Ryan Odribege <rodribege@gmail.com>
2021-05-14 13:25:19 -0700

    changed app.js to be able to use .css files

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 let app = express();
     app.use(express.urlencoded({extended: true}));
-    app.use(express.static("public"));
+    app.use(express.static("public/styles"));
     app.set("view engine", "ejs");
     
 let PORT = process.env.PORT || 3000;
