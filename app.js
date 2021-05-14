fbcd2f537a06b523d5e82a6c1db6043e90ac6375
Ryan Odribege <rodribege@gmail.com>
2021-05-14 15:01:26 -0700

    Trying yet another path for the favicon

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const { render } = require("ejs");
 const express = require("express");
+const favicon = require('serve-favicon');
 
 
 let app = express();
@@ -7,6 +8,7 @@ let app = express();
     app.use("/public", express.static('public')); 
     app.use("/styles", express.static("styles"));
     app.use("/images", express.static("images"));
+    app.use(favicon(path.join(__dirname,'public','favicon-package','favicon.ico')));
     app.set("view engine", "ejs");
 
     
